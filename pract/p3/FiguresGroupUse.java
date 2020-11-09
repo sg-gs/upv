e7721e75a95cb563e7597f2a862f4bf35523bbcc
SergioGutierrezVillalba <gv.sergio@gmail.com>
2020-11-09 22:31:59 +0100

    LTP/pract/p3

---
+++ pract/p3/FiguresGroupUse.java
@@ -0,0 +1,23 @@
+package practica3;
+
+
+/**
+ * class FiguresGroupUse.
+ * 
+ * @author LTP 
+ * @version 2020-21
+ */
+public class FiguresGroupUse {
+    public static void main(String[] args) {
+        FiguresGroup g = new FiguresGroup();
+        g.add(new Circle(1.0, 6.0, 6.0));
+        g.add(new Rectangle(2.0, 5.0, 10.0, 12.0));
+        g.add(new Triangle(3.0, 4.0, 10.0, 2.0));
+        g.add(new Circle(4.0, 3.0, 1.0));
+        g.add(new Triangle(5.0, 1.0, 1.0, 2.0));
+        g.add(new Square(2.0, 2.0, 2.0, 2.0));
+        g.add(new Rectangle(7.0, 2.0, 1.0, 3.0));
+        System.out.println(g.orderedList());
+        g.print('a');
+    }
+}
---
+++ pract/p3/Rectangle.java
@@ -0,0 +1,63 @@
+package practica3;
+
+import java.lang.Math;
+
+public class Rectangle extends Figure implements ComparableRange, Printable
+{
+    private double base; 
+    private double height;
+
+    /**
+     * Constructor for objects of class Rectangle
+     */
+    public Rectangle(double x, double y, double b, double h)
+    {
+        super(x, y);
+        base = b;
+        height = h;
+    }
+    
+    public boolean equals(Object o) {
+        if(super.equals(o) && (o instanceof Rectangle)) {
+            Rectangle r = (Rectangle) o;
+            return height == r.height && base == r.base;
+        }
+        return false;
+    }
+
+    public String toString() {
+        return "Rectangle:\n\t" +
+            super.toString() +
+            "\n\tBase: " + base +
+            "\n\tHeight: " + height;
+    }
+
+    public double area () {
+        return base * height;
+    }
+    
+
+    public int compareToRange(Figure f) {
+        double areasSum = f.area() + this.area();
+        double areasDiff = Math.abs(f.area() - this.area());
+        
+        if(areasDiff <= (0.1 * areasSum)) {
+            return 0;
+        } else {
+            return this.compareTo(f);
+        }
+    }
+    
+    public void print(char c) {
+        int b = (int) this.base;
+        int h = (int) this.height;
+        
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < b; j++){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+        
+    }
+}
