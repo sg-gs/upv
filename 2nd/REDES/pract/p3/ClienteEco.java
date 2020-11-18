e69efba916f2a48e601ea543195314f1788b9ffe
SergioGutierrezVillalba <gv.sergio@gmail.com>
2020-11-18 21:25:23 +0100

    added pract folder

---
+++ 2nd/REDES/pract/p3/ClienteEco.java
@@ -0,0 +1,56 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class ClienteEco
+{
+	public static void main(String args[]) 
+	{
+		ClienteEco ct2 = new ClienteEco();
+    	}
+
+	public ClienteEco()
+	{
+		this.connect();
+	}
+
+    	public void connect() 
+    	{
+		try {
+			Socket s = new Socket("zoltar.redes.upv.es", 7);
+			System.out.println("Conectado!");
+
+			// PrintWriter pw = new PrintWriter(s.getOutputStream(), false);
+			// pw.println("¡¡Hola Mundo!!");
+			
+			// Primera corrección
+			// PrintWriter pw = new PrintWriter(s.getOutputStream(), false);
+			// pw.println("¡¡Hola Mundo!!");
+			// pw.flush();
+
+			// Segunda corrección, con autoFlush
+			PrintWriter pw = new PrintWriter(s.getOutputStream(), true);
+			pw.println("¡¡Hola Mundo!!");
+
+			Scanner zoltar = new Scanner(s.getInputStream());
+			while(zoltar.hasNext()) 
+			{
+				System.out.println(">> zoltar.redes.upv.es: " + zoltar.nextLine());
+			}
+			
+			s.close();
+		} 
+		catch (UnknownHostException uhe)
+		{
+			System.out.println("Nombre de servidor desconocido");
+		} 
+		catch (ConnectException ioe)
+		{
+			System.out.println("No es posible realizar la conexión");
+		} 
+		catch (Exception e)
+		{
+			System.out.println("Unknown exception" + e.getMessage());		
+		}
+	}
+}
---
+++ 2nd/REDES/pract/p3/ClienteHTTP.java
@@ -0,0 +1,50 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class ClienteHTTP
+{
+	public static void main(String args[]) 
+	{
+		ClienteHTTP ct2 = new ClienteHTTP();
+    	}
+
+	public ClienteHTTP()
+	{
+		this.connect();
+	}
+
+    	public void connect() 
+    	{
+		try {
+			Socket s = new Socket("www.upv.es", 80);
+			System.out.println("Conectado!");
+
+			// Enviar Request
+			PrintWriter pw = new PrintWriter(s.getOutputStream(), true);
+			pw.printf("GET / HTTP/1.0\r\n\r\n");
+
+			// Recibir respuesta
+			Scanner upv = new Scanner(s.getInputStream());
+
+			while(upv.hasNextLine())
+			{
+				System.out.println(">> upv.es: " + upv.nextLine());
+			}
+
+			s.close();
+		} 
+		catch (UnknownHostException uhe)
+		{
+			System.out.println("Nombre de servidor desconocido");
+		} 
+		catch (ConnectException ioe)
+		{
+			System.out.println("No es posible realizar la conexión");
+		} 
+		catch (Exception e)
+		{
+			System.out.println("Unknown exception" + e.getMessage());		
+		}
+	}
+}
