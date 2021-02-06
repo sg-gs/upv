1608b5f6a42a8fabc5a3231721d364854bee7374
SergioGutierrezVillalba <gv.sergio@gmail.com>
2021-02-06 20:20:05 +0100

    keep buffer size instead of cutting msgs

--- 2nd/REDES/pract/p7/DatagramPacketClientListener.java
+++ 2nd/REDES/pract/p7/DatagramPacketClientListener.java
@@ -1,6 +1,3 @@
-
-
-import java.io.*;
 import java.net.*;
 
 public class DatagramPacketClientListener
@@ -10,13 +7,23 @@ public class DatagramPacketClientListener
 		try 
 		{
 			String msg = new String("eee\n");
-			DatagramPacket dp = new DatagramPacket(msg.getBytes(), msg.getBytes().length, InetAddress.getByName("localhost"), 7777);
+			byte [] buf = new byte[256];
+			byte [] msgBytes = msg.getBytes();
+
+			for(int i = 0; i < msg.length(); i++) {
+				buf[i] = msgBytes[i]; // refill buffer but keeping 256 as the size
+			}
+
+			DatagramPacket dp = new DatagramPacket(buf, buf.length, InetAddress.getByName("localhost"), 7777);
 			DatagramSocket ds = new DatagramSocket();
+
 			ds.send(dp);
-			ds.receive(dp);
-			String response = new String(dp.getData(), 0, dp.getLength());
+			ds.receive(dp); // blocking
+
+			String response = new String(dp.getData(), 0, dp.getLength()); // get length will return 256. So we can receive a message up to 256 chars
 			System.out.println("RESPONSE: " + response);
-		
+
+			ds.close(); // don't waste resources!
 		} catch (Exception e) {
 			System.out.println("Exception" + e.getMessage());
 		}
