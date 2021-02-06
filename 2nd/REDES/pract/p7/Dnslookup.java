e53b8632b37f404bd9d14b3534ed0e0bc0a4afae
SergioGutierrezVillalba <gv.sergio@gmail.com>
2021-02-06 19:26:44 +0100

    dnslookup refactor

--- 2nd/REDES/pract/p7/Dnslookup.java
+++ 2nd/REDES/pract/p7/Dnslookup.java
@@ -1,24 +1,24 @@
-
-
-import java.io.*;
 import java.net.*;
 
 public class Dnslookup
 {
 	public static void main(String args[])
 	{
-		try 
-		{
+		try {
 			String host = args[0];
-			System.out.println("HOST: " + host);
-			System.out.println("IP: " + InetAddress.getByName(host).toString().split("/")[1]);
-		
-		} catch (UnknownHostException uhe) {
-			System.out.println("Unknown host");
-		} catch (Exception e) {
-			System.out.println("Exception" + e.getMessage());
-		}
-		
-	}	
 
+			try {
+				System.out.println("HOST: " + host);
+				System.out.println("IP: " + InetAddress.getByName(host).toString().split("/")[1]);
+			} catch (UnknownHostException uhe) {
+				System.out.println("Unknown host " + host);
+			} catch (Exception e) {
+				System.out.println("Unexpected exception: " + e.getMessage());
+			}
+
+		} catch (ArrayIndexOutOfBoundsException aibe) {
+			System.out.println(("Provide a host!"));
+			System.out.println("Program usage: 'java Dnslookup ${host}'");
+		}
+	}	
 }
