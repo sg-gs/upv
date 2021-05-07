9489438bbdef72492ea34c83b53daaa6bd95d812
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-05-07 19:18:57 +0200

    add CSD/p3

---
+++ 2nd/CSD/pract/p3/Ants/Op.java
@@ -0,0 +1,19 @@
+
+/**
+ * Base class for simulation ops
+ * 
+ * @author CSD Juansa Sendra
+ * @version 2021
+ */
+public class Op {
+    String ch = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public int a;
+
+    public Op(int a) {
+        this.a = a;
+    }
+
+    public String toString() {
+        return "" + ch.charAt(a);
+    }
+}
---
+++ 2nd/CSD/pract/p3/Ants/Retry.java
@@ -0,0 +1,16 @@
+
+/**
+ * Ant retries a movement
+ * 
+ * @author CSD Juansa Sendra
+ * @version 2021
+ */
+public class Retry extends Op {
+    public Retry(int a) {
+        super(a);
+    }
+
+    public String toString() {
+        return super.toString() + " Retry";
+    }
+}
