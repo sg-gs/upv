379cb01112c23595e263def4362d5e3aef54ebd9
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-10-12 09:02:33 +0200

    add EDA last course changes

--- 2nd/EDA/eda/librerias/estructurasDeDatos/modelos/ColaPrioridad.java
+++ 2nd/EDA/eda/librerias/estructurasDeDatos/modelos/ColaPrioridad.java
@@ -4,27 +4,24 @@ package librerias.estructurasDeDatos.modelos;
  * Modelo de una Cola de Prioridad, o de Busqueda Dinamica 
  * del Elemento de maxima prioridad en una Coleccion
  * 
+ * @version Febrero 2018
+ * @param <E> tipo de datos de la estructura,  
+ * RESTRINGIDO POR Comparable 
  */
 
 public interface ColaPrioridad<E extends Comparable<E>> {
-// metodos Modificadores del estado de una Cola de Prioridad (CP):
-    /** atendiendo a su prioridad, 
-     *  inserta el Elemento e en una Cola de Prioridad 
-     */
+
+    // Metodos Modificadores del estado de una Cola de Prioridad (CP)
+    /** Atendiendo a su prioridad, inserta el Elemento e en una CP */
     void  insertar(E e);
-    
     /** SII !esVacia(): 
-     *  obtiene y elimina el Elemento con maxima prioridad de una CP 
-     */
+     *  obtiene y elimina el Elemento con maxima prioridad de una CP */
     E  eliminarMin();
     
-// metodos Consultores del estado de una Cola de Prioridad (CP):
+    // Metodos Consultores del estado de una Cola de Prioridad (CP)
     /** SII !esVacia(): 
-     *  obtiene el Elemento con maxima prioridad de una CP 
-     */
+     *  obtiene el Elemento con maxima prioridad de una CP */
     E  recuperarMin();
-    
-    /** comprueba si una CP esta vacía 
-     */
+    /** Comprueba si una CP esta vacia */
     boolean esVacia();
 }
