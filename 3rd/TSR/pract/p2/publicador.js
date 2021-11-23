4653894aee4c892c161f0fcb154a825274ea16e1
sergiogutierrezvillalba <serguvil@upv.edu.es>
2021-11-23 21:44:54 +0100

    p2/publicador

---
+++ 3rd/TSR/pract/p2/publicador.js
@@ -0,0 +1,42 @@
+const { socket } = require('zeromq');
+
+const [_, __, port, nMessages, ...topics] = process.argv;
+
+const pub = socket('pub').bind('tcp://*:' + port);
+
+const topicsHistory = topics.map((topic) => ({
+  label: topic,
+  counter: 1
+}));
+
+let seconds = 1;
+
+const emitMsg = () => {
+  const topic = topicsHistory[0];
+
+  /* Laziness: check first the content is the expected */
+  /* console.log(`${seconds}: ${topic.label} ${topic.counter++}`); */
+
+  pub.send(`${seconds}: ${topic.label} ${topic.counter++}`);
+  topicsHistory.shift(); 
+  topicsHistory.push(topic); 
+}
+
+socket('sub').connect('tcp://127.0.0.1:' + port)
+  .subscribe('')
+  .on('message', (m) => console.log(m.toString()));
+
+const intervalId = setInterval(() => {
+  emitMsg();
+
+  if (seconds === parseInt(nMessages)) {
+    /* Allow sub to receive last message before end. */
+    setTimeout(() => {
+      clearInterval(intervalId);
+      console.log('Program finished');
+      process.exit(0);
+    }, 500);    
+  }
+
+  seconds++;
+}, 1000);
